036933baac9797eee3f8db4a213d02e2a2b63117
zono0013 <143684883+zono0013@users.noreply.github.com>
2024-06-01 15:12:08 +0900

    SECRET_KEY追加

--- azure_project/deployment.py
+++ azure_project/deployment.py
@@ -4,6 +4,7 @@ from .settings import *
 from .settings import BASE_DIR
 
 # Azure deployment settings
+SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
